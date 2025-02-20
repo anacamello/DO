ef60034895b43e01cd0399837ed0f23c093d7e8b
anacamello <ana.camello@gmail.com>
2025-02-20 07:11:52 -0300

    Update DO_streamlit.py

--- DO_streamlit.py
+++ DO_streamlit.py
@@ -556,6 +556,8 @@ def identifica_orgao2(nomear_designar, subtexto_paragrafo, orgao1):
     else:
 
         if("da Fundação" in subtexto_paragrafo):
+            
+            st.text("Fundação")
 
             orgao2 = "Fundação" + subtexto_paragrafo.partition("da Fundação")[2].split(".")[0]
 
